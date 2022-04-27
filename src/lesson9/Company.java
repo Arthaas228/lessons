09e853d737c7462729151612ec551329f28113b1
Andrey <Arthaas228@gmail.com>
2022-04-27 20:15:02 +0300

    task11

---
+++ src/lesson9/Company.java
@@ -0,0 +1,33 @@
+package lesson9;
+
+public class Company {
+    private String name;
+    private String countryFounded;
+
+    protected String code;
+
+    public int someField;
+
+    public Company(String name, String countryFounded) {
+        this.name = name;
+        this.countryFounded = countryFounded;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getCoutryFounded(){
+        return countryFounded;
+    }
+
+
+
+
+    public String getCountryFounded() {
+        return countryFounded;
+    }
+
+    public void setCountryFounded(String countryFounded) {
+        this.countryFounded = countryFounded;
+    }
+}
