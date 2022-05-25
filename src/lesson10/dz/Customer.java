0441ecd59e4f3a3824f53ecd2587b5d1a3af9472
Andrey <Arthaas228@gmail.com>
2022-05-25 14:32:36 +0300

    task11

--- src/lesson10/dz/Customer.java
+++ src/lesson10/dz/Customer.java
@@ -1,7 +1,27 @@
 package lesson10.dz;
 
 public class Customer {
-    String name;
-    String city;
-    String gender;
+    private String name;
+    private String city;
+    private String gender;
+
+
+    public Customer(String name, String city, String gender) {
+        this.name = name;
+        this.city = city;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
 }
