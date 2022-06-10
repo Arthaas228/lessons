cd64fba63f4e0528212bc207ea85b471ea2e8306
Andrey <Arthaas228@gmail.com>
2022-06-10 13:15:31 +0300

    task11

--- src/lesson11/bigInterfaceExample/SimpleReader.java
+++ src/lesson11/bigInterfaceExample/SimpleReader.java
@@ -1,10 +1,19 @@
 package lesson11.bigInterfaceExample;
 
-public class SimpleReader implements Readable{
+public class SimpleReader implements Readable {
+    static String test;
+
+    public static void main(String[] args) {
+        System.out.println(test);
+    }
+
     @Override
     public void readFileStorage(Storage storage) {
-        for(File file : storage.getFiles()){
-            System.out.println(file.getName());
+        for (File file : storage.getFiles()) {
+            if(file != null)
+                System.out.println(file.getName());
+            else
+                System.out.println(file);
         }
 
 
